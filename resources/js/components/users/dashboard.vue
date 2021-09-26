fec4dece25900032d162f3e2962bc17d1a1e8bf2
dineshranga95 <dineshranga570@gmail.com>
2021-09-26 21:28:53 +0530

    use vuejs inside laravel project

---
+++ resources/js/components/users/dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="card mt-3">
+            <div class="card-body">
+                <h3>Manage Users</h3>
+                <table class="table table-hover">
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>User Since</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr>
+
+                    </tr>
+                </table>
+            </div>
+        </div> 
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')    
+        <user-management-dashboard></user-management-dashboard>             
+    
+@endsection
